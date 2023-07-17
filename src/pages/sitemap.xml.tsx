5063eac5eba6fc51c47f8a0926a783290b4fe818
Fedi Kouzana <fedi.kouzana1@gmail.com>
2023-07-17 12:28:45 +0100

    sitemap improv + cursor none

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -6,15 +6,21 @@ function generateSiteMap(aliasList: { alias: string }[]) {
   <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>${DOMAIN_NAME}</loc>
+       <changefreq>monthly</changefreq>
+       <priority>1</priority>
      </url>
      <url>
        <loc>${DOMAIN_NAME}projects</loc>
+       <changefreq>monthly</changefreq>
+       <priority>0.8</priority>
      </url>
      ${aliasList
        .map(({ alias }) => {
          return `
        <url>
            <loc>${`${DOMAIN_NAME}projects/${alias}`}</loc>
+           <changefreq>monthly</changefreq>
+           <priority>0.6</priority>
        </url>
      `;
        })
